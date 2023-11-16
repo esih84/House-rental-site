48967783b8af9817108537c5a91fe4f16ab8990c
esih84 <esmaeilh.haji84@gmail.com>
2023-11-16 13:39:05 +0330

    create model profile

---
+++ models/Profile.js
@@ -0,0 +1,55 @@
+import { model, models, Schema } from "mongoose";
+
+
+const ProfileSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    realestate:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        enum:["villa","apartment","store","office"],
+        required:true
+    },
+    constructionDate:{
+        type:Date,
+        required:true,
+    },
+    amenities:{
+        type:[String],
+        default:[],
+    },
+    rules:{
+        type:[String],
+        default:[],
+    },
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+},{timestamps:true})
+
+const Profile = models.Profile || model("Profile", userSchema);
+
+export default User;
