924ef2dce9d6e034aaab2c88723babecb24adf1a
esih84 <esmaeilh.haji84@gmail.com>
2023-11-16 13:21:32 +0330

    '

--- components/modules/CustomDatePicker.js
+++ components/modules/CustomDatePicker.js
@@ -4,21 +4,21 @@ import persian_fa from "react-date-object/locales/persian_fa"
 const CustomDatePicker = ({profileData,setProfileData}) => {
     const changeHandler =(e)=>{
         setProfileData({...profileData,constructionDate:new Date(e)})
-        console.log(new Date(e))
+        // console.log(e)
+        // console.log(new Date(e))
     }
     return (
-        <div>
-            <p>تاریخ ساخت</p>
-            <div>
+        <div className=" mb-16">
+            <p className="mb-[5px]">تاریخ ساخت</p>
             <DatePicker 
                 calendar={persian}
                 locale={persian_fa}
                 calendarPosition="bottom-right"
                 value={profileData.constructionDate}
                 onChange={changeHandler}
+                style={{width:"110px", border:"1px dashed #304ffe",color:"gray",padding:"10px",height:"30px",marginLeft:"10px", fontSize:"1rem", textAlign:"center", borderRadius:"4px"}}
             />
 
-            </div>
         </div>
     );
 }
