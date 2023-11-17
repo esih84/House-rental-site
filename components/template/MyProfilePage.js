c3024a2f4b8a58d2297471f67eb3bbf3badf0c14
esih84 <esmaeilh.haji84@gmail.com>
2023-11-17 07:36:23 +0330

    Develop Card and Dashboard Card

--- components/template/MyProfilePage.js
+++ components/template/MyProfilePage.js
@@ -6,7 +6,7 @@ const MyProfilePage = ({profiles}) => {
         <div>
             {profiles.length?null:<p className=" text-red-600 bg-red-900 text-[1.3rem] py-[10px] px-[15px] rounded-[10px]">هیچ آگهی ثبت نشده است</p>}
             {profiles.map(profile=>(
-                <DashboardCard key={profile._id} data={profile}/>
+                <DashboardCard key={profile._id} data={JSON.parse(JSON.stringify(profile))}/>
             ))}
         </div>
     );
