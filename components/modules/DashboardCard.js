a0a7c95dde097910c1b62fed5467e4bfee25827c
esih84 <esmaeilh.haji84@gmail.com>
2023-11-17 07:36:23 +0330

    Develop Card and Dashboard Card

--- components/modules/DashboardCard.js
+++ components/modules/DashboardCard.js
@@ -1,6 +1,19 @@
+'use client';
+import Card from "./Card";
+import { FiEdit } from "react-icons/fi";
+import { AiOutlineDelete } from "react-icons/ai";
+
 const DashboardCard = ({data}) => {
+    const editHandler=()=>{}
+    const deleteHandler=()=>{}
     return (
-        <div>{data.title}</div>
+        <div className="flex border-2 border-[#304ffe58] rounded-2xl mb-5">
+            <Card data={data}/>
+            <div className="flex items-end p-[10px] w-full gap-[10px]">
+                <button onClick={editHandler} className="flex justify-center items-center  w-[48%] bg-white cursor-pointer h-10 rounded-lg text-xs sm:text-base border border-green-600 text-green-600 ">ویرایش<FiEdit className=" text-sm sm:text-[1.1rem] "/></button>
+                <button onClick={deleteHandler} className="flex justify-center items-center w-[48%] bg-white cursor-pointer h-10 rounded-lg text-xs sm:text-base border border-red-600 text-red-600">حذف آگهی<AiOutlineDelete className=" text-sm sm:text-[1.1rem] "/></button>
+            </div>
+        </div>
     );
 }
 
